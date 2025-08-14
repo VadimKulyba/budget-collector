2528ef007e1a378828940174ce3763d822858b82
Vadim Kulyba <kulyba.vadim@gmail.com>
2025-08-14 20:52:10 +0300

    clean up

--- pkg/banking/pjcbby2x/collector.go
+++ pkg/banking/pjcbby2x/collector.go
@@ -21,6 +21,21 @@ const (
 	periodLabel = "Период выписки:"
 )
 
+const (
+	operationNameKey     = "Операция"
+	operationSumKey      = "Сумма"
+	operationDateKey     = "Дата операции по счету"
+	operationCategoryKey = "Категория операции"
+)
+
+// excluded operations
+const (
+	topUpOperation            = "CH Payment To Client Contract"
+	moneyTransferOperation    = "BLR MINSK MOBILE BANK"
+	serviceOperation          = "CH Payment BLR MINSK P2P SDBO NO FEE"
+	internalTransferOperation = "CH Debit BLR MINSK P2P SDBO NO FEE"
+)
+
 func FindReportByHeaderPeriod(period string) (string, error) {
 	periodRange := datetime.GetMonthRangeByPeriod(period)
 	reports, err := filepath.Glob(reportCollectionMask)
@@ -51,10 +66,12 @@ func CollectMonthlyReport(records [][]string) []models.MonthlyReportOperation {
 	var currentPaymentMethod ReportPaymentMethodStat
 
 	for index, value := range records {
+		// Header marker
 		if len(value) == 1 && strings.Contains(value[0], "Операции по ........") {
 			parts := strings.Split(value[0], "........")
 			currentPaymentMethod.last4 = parts[len(parts)-1]
 			currentPaymentMethod.headerIndex = uint16(index + 1)
+			// Last line marker
 		} else if len(value) > 0 && strings.Contains(value[0], "Всего по контракту") {
 			currentPaymentMethod.lastLineIndex = uint16(index - 1)
 			paymentMethods = append(paymentMethods, currentPaymentMethod)
@@ -71,19 +88,24 @@ func CollectMonthlyReport(records [][]string) []models.MonthlyReportOperation {
 		}
 
 		for i := pm.headerIndex + 1; i <= pm.lastLineIndex; i++ {
-			operationName := records[i][headerMap["Операция"]]
-			operationCost := currency.StrToMoney(records[i][headerMap["Сумма"]])
+			operationName := records[i][headerMap[operationNameKey]]
+			operationCost := currency.StrToMoney(records[i][headerMap[operationSumKey]])
 
-			if operationCost < 0 && !strings.Contains(operationName, "BLR MINSK MOBILE BANK") && !strings.Contains(operationName, "CH Payment To Client Contract") && !strings.Contains(operationName, "CH Debit BLR MINSK P2P SDBO NO FEE") {
+			excludedOperations := strings.Contains(operationName, topUpOperation) ||
+				strings.Contains(operationName, moneyTransferOperation) ||
+				strings.Contains(operationName, serviceOperation) ||
+				strings.Contains(operationName, internalTransferOperation)
+
+			if operationCost < 0 && !excludedOperations {
 
 				operation := models.MonthlyReportOperation{
 					Name:        operationName,
-					Date:        records[i][headerMap["Дата операции по счету"]],
+					Date:        records[i][headerMap[operationDateKey]],
 					PaymentType: models.Card,
-					Category:    CategoryMap[records[i][headerMap["Категория операции"]]],
-					Subcategory: "",
+					Category:    CategoryMap[records[i][headerMap[operationCategoryKey]]],
+					Subcategory: "", // TODO
 					Cost:        math.Abs(operationCost),
-					Currency:    "BYN",
+					Currency:    "BYN", // TODO
 					Last4:       pm.last4,
 				}
 
